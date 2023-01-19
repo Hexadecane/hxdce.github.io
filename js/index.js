658ebc94e3fd7ab80ccdc0a2b4de620b658749c4
Hexadecane <36948730+Hexadecane@users.noreply.github.com>
2023-01-18 23:14:57 -0500

    Adjust JS code formatting

--- js/index.js
+++ js/index.js
@@ -3,12 +3,24 @@
 let workSectionIndex = 0;
 
 function makeHTTPObject() {
-    try {return new XMLHttpRequest();}
-    catch (error) {}
-    try {return new ActiveXObject('Msxml2.XMLHTTP');}
-    catch (error) {}
-    try {return new ActiveXObject('Microsoft.XMLHTTP');}
-    catch (error) {}
+    try {
+        return new XMLHttpRequest();
+    }
+    catch (error) {
+
+    }
+    try {
+        return new ActiveXObject('Msxml2.XMLHTTP');
+    }
+    catch (error) {
+
+    }
+    try {
+        return new ActiveXObject('Microsoft.XMLHTTP');
+    }
+    catch (error) {
+        
+    }
 
     throw new Error('Could not create HTTP request object.');
 }
